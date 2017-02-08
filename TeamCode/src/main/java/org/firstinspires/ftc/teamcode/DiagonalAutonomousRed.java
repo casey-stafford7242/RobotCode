ac01ec3a46a8ba982b2551fa104f5af5394b70de
Casey Stafford <casey.stafford@bridgecreek.k12.ok.us>
2017-02-08 07:49:05 -0600

    Added Red Color Autonomous Programs for Diagonal and Turning

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DiagonalAutonomousRed.java
@@ -0,0 +1,416 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.GyroSensor;
+import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+/**
+ * Created by casey stafford on 2/8/2017.
+ */
+
+public class DiagonalAutonomousRed extends OpMode
+{
+    DcMotor backLeftMotor, backRightMotor, frontLeftMotor, frontRightMotor, whiskMotor, leftShootMotor, rightShootMotor;
+    Servo leftButtonPushServo;
+    ColorSensor leftButtonPushColorSensor;
+    GyroSensor gyro;
+    OpticalDistanceSensor colorSensor;
+    boolean timeBeenSet = false;
+    boolean buttonPressed = false;
+    double speedCheckStartTime;
+    int currentCounts;
+    private static final int MAX_MOTOR_RPM = 77;
+    boolean speedCheckTrigger = false;
+    double startMethodTime = 0;
+    BotState curState;
+
+
+    public enum BotState
+    {
+        SHOOT_PARTICLES, INITIAL_TURN, DRIVE_TO_WALL, TURNS_WITH_WALL, FIND_WHITE_LINE, PUSH_BUTTON
+    }
+    @Override
+    public void init()
+    {
+        frontRightMotor = hardwareMap.dcMotor.get("frontRightMotor");
+        frontLeftMotor = hardwareMap.dcMotor.get("frontLeftMotor");
+        backRightMotor = hardwareMap.dcMotor.get("backRightMotor");
+        leftButtonPushServo = hardwareMap.servo.get("leftButtonPushServo");
+        backLeftMotor = hardwareMap.dcMotor.get("backLeftMotor");
+        colorSensor = hardwareMap.opticalDistanceSensor.get("colorSensor");
+        leftShootMotor = hardwareMap.dcMotor.get("leftShootMotor");
+        rightShootMotor = hardwareMap.dcMotor.get("rightShootMotor");
+        whiskMotor = hardwareMap.dcMotor.get("whiskMotor");
+        leftButtonPushColorSensor = hardwareMap.colorSensor.get("leftButtonPushColorSensor");
+        //rightButtonPushServo.setPosition(0);
+        leftButtonPushServo.setPosition(1);
+        // leftTouchSensor = hardwareMap.get(ModernRoboticsDigitalTouchSensor.class, "leftTouchSensor");
+        // rightTouchSensor = hardwareMap.get(ModernRoboticsDigitalTouchSensor.class, "rightTouchSensor");
+        gyro = hardwareMap.gyroSensor.get("gyro");
+        gyro.calibrate();
+        frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightShootMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        curState = BotState.SHOOT_PARTICLES;
+    }
+
+    @Override
+    public void loop()
+    {
+        switch (curState)
+        {
+            case SHOOT_PARTICLES:
+                shootParticles();
+                break;
+            case INITIAL_TURN:
+                gyroTurn(-.3, 315, 5);
+                break;
+            case DRIVE_TO_WALL:
+                driveUsingTime(2, .5);
+                break;
+            case TURNS_WITH_WALL:
+                gyroTurn(.3, 0, 5);
+                break;
+            case FIND_WHITE_LINE:
+                runToWhiteLine(.05, .3);
+                break;
+            case PUSH_BUTTON:
+                pushButton();
+                break;
+        }
+    }
+
+    public void sleep(long time)
+    {
+        //Faster than writing the exception handling each time
+        try
+        {
+            Thread.sleep(time);
+        }
+        catch (InterruptedException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public void gyroTurn(double motorPower, int gyroTarget, int TOLERANCE)
+    {
+        if(gyro.getHeading() < gyroTarget + TOLERANCE && gyro.getHeading() > gyroTarget - TOLERANCE)
+        {
+            backLeftMotor.setPower(0);
+            backRightMotor.setPower(0);
+            frontRightMotor.setPower(0);
+            frontLeftMotor.setPower(0);
+            if(curState == BotState.INITIAL_TURN)
+            {
+                curState = BotState.DRIVE_TO_WALL;
+            }
+            else
+            {
+                curState = BotState.FIND_WHITE_LINE;
+            }
+            sleep(500);
+        }
+        else if (gyro.getHeading() > gyroTarget + TOLERANCE)
+        {
+            backLeftMotor.setPower(-motorPower);
+            backRightMotor.setPower(motorPower);
+            frontLeftMotor.setPower(-motorPower);
+            frontRightMotor.setPower(motorPower);
+        }
+        else if (gyro.getHeading() < gyroTarget - TOLERANCE)
+        {
+            backLeftMotor.setPower(motorPower);
+            backRightMotor.setPower(-motorPower);
+            frontLeftMotor.setPower(motorPower);
+            frontRightMotor.setPower(-motorPower);
+        }
+    }
+
+
+    public void alignWithWall()
+    {
+        if(gyro.getHeading() > 180)
+        {
+            backLeftMotor.setPower(.2);
+            frontLeftMotor.setPower(.2);
+            backRightMotor.setPower(-.2);
+            frontRightMotor.setPower(-.2);
+            //alignFourMotorSpeed(backLeftMotor, backRightMotor, frontLeftMotor, frontRightMotor);
+        }
+        else if (gyro.getHeading() < 180 && gyro.getHeading() > 0)
+        {
+            backRightMotor.setPower(.2);
+            frontRightMotor.setPower(.2);
+            backLeftMotor.setPower(-.2);
+            frontLeftMotor.setPower(-.2);
+            //alignFourMotorSpeed(backRightMotor, frontRightMotor, backLeftMotor, frontLeftMotor);
+        }
+
+        if(gyro.getHeading() == 0)
+        {
+            backRightMotor.setPower(0);
+            backLeftMotor.setPower(0);
+            frontLeftMotor.setPower(0);
+            frontRightMotor.setPower(0);
+        }
+    }
+
+    public void driveUsingTime(double timeCheck, double motorPower)
+    {
+        if(startMethodTime == 0 && timeBeenSet == false)
+        {
+            startMethodTime = time;
+            timeBeenSet = true;
+        }
+        if(timeBeenSet == true && time < startMethodTime + timeCheck)
+        {
+            backLeftMotor.setPower(motorPower);
+            backRightMotor.setPower(motorPower);
+            frontLeftMotor.setPower(motorPower);
+            frontRightMotor.setPower(motorPower);
+            //alignFourMotorSpeed(backLeftMotor, backRightMotor, frontLeftMotor, frontRightMotor);
+        }
+        else if (timeBeenSet == true && time >= startMethodTime + timeCheck)
+        {
+            backLeftMotor.setPower(0);
+            backRightMotor.setPower(0);
+            frontLeftMotor.setPower(0);
+            frontRightMotor.setPower(0);
+            startMethodTime = 0;
+            timeBeenSet = false;
+            if(curState == BotState.DRIVE_TO_WALL)
+            {
+                curState = BotState.TURNS_WITH_WALL;
+            }
+            else
+            {
+
+            }
+            sleep(500);
+        }
+    }
+
+    public void pushButton()
+    {
+        telemetry.addData("Red", leftButtonPushColorSensor.red());
+        telemetry.addData("Blue", leftButtonPushColorSensor.blue());
+        if(leftButtonPushColorSensor.red() > leftButtonPushColorSensor.blue())
+        {
+            backLeftMotor.setPower(0);
+            backRightMotor.setPower(0);
+            frontLeftMotor.setPower(0);
+            frontRightMotor.setPower(0);
+            leftButtonPushServo.setPosition(.7);
+            curState = BotState.FIND_WHITE_LINE;
+            sleep(500);
+            buttonPressed = true;
+        }
+        else
+        {
+            driveUsingTime(2, .15);
+        }
+    }
+
+    public void strafeUsingTime(double timeCheck, double motorPower)
+    {
+        if(startMethodTime == 0 && timeBeenSet == false)
+        {
+            startMethodTime = time;
+            timeBeenSet = true;
+        }
+        if(timeBeenSet == true && time < startMethodTime + timeCheck)
+        {
+            backLeftMotor.setPower(-motorPower);
+            backRightMotor.setPower(0);
+            frontLeftMotor.setPower(motorPower);
+            frontRightMotor.setPower(0);
+            //alignFourMotorSpeed(backLeftMotor, backRightMotor, frontLeftMotor, frontRightMotor);
+
+        }
+        else if (timeBeenSet == true && time >= startMethodTime + timeCheck)
+        {
+            backLeftMotor.setPower(0);
+            backRightMotor.setPower(0);
+            frontLeftMotor.setPower(0);
+            frontRightMotor.setPower(0);
+            timeBeenSet = false;
+            startMethodTime = 0;
+            if(gyro.getHeading() > 5)
+            {
+                frontLeftMotor.setPower(-.2);
+                frontRightMotor.setPower(.2);
+                backLeftMotor.setPower(-.2);
+                backRightMotor.setPower(.2);
+            }
+            curState = BotState.FIND_WHITE_LINE;
+        }
+    }
+
+    public void alignFourMotorSpeed(DcMotor motorOne, DcMotor motorTwo, DcMotor motorThree, DcMotor motorFour)
+    {
+        if(motorSpeed(motorOne) != motorSpeed(motorTwo))
+        {
+            alignMotorSpeed(motorOne, motorTwo);
+        }
+        if(motorSpeed(motorThree) != motorSpeed(motorFour))
+        {
+            alignMotorSpeed(motorThree, motorFour);
+        }
+        if(motorSpeed(motorOne) == motorSpeed(motorTwo) && motorSpeed(motorThree) == motorSpeed(motorFour) && motorSpeed(motorTwo) != motorSpeed(motorThree))
+        {
+            alignMotorSpeed(motorTwo, motorThree);
+        }
+    }
+
+    public void runToWhiteLine(double driveUsingTimeTriggerVal, double motorPower)
+    {
+        if(colorSensor.getLightDetected() > driveUsingTimeTriggerVal)
+        {
+            backRightMotor.setPower(0);
+            backLeftMotor.setPower(0);
+            frontLeftMotor.setPower(0);
+            frontRightMotor.setPower(0);
+            buttonPressed = false;
+            curState = BotState.PUSH_BUTTON;
+            sleep(500);
+        }
+        else
+        {
+            backRightMotor.setPower(motorPower);
+            backLeftMotor.setPower(motorPower);
+            frontRightMotor.setPower(motorPower);
+            frontLeftMotor.setPower(motorPower);
+            if(buttonPressed == true)
+            {
+                leftButtonPushServo.setPosition(0);
+            }
+        }
+    }
+
+    public void shootParticles()
+    {
+        if(time <= 3)
+        {
+            leftShootMotor.setPower(.25);
+            rightShootMotor.setPower(.25);
+            if(motorSpeed(leftShootMotor) > motorSpeed(rightShootMotor))
+            {
+                telemetry.addData("Aligning Motor Speed", "MotorOne > MotorTwo");
+                leftShootMotor.setPower(leftShootMotor.getPower() - ((rpmToPowerConverter(motorSpeed(leftShootMotor)) - rpmToPowerConverter(motorSpeed(rightShootMotor))) / 2));
+                rightShootMotor.setPower(rightShootMotor.getPower() + ((rpmToPowerConverter(motorSpeed(leftShootMotor)) - rpmToPowerConverter(motorSpeed(rightShootMotor))) / 2));
+            }
+            else if (motorSpeed(leftShootMotor) < motorSpeed(rightShootMotor))
+            {
+                telemetry.addData("Aligning Motor Speed", "MotorTwo > MotorOne");
+                rightShootMotor.setPower(rightShootMotor.getPower() - ((rpmToPowerConverter(motorSpeed(rightShootMotor)) - rpmToPowerConverter(motorSpeed(leftShootMotor))) / 2));
+                leftShootMotor.setPower(leftShootMotor.getPower() + ((rpmToPowerConverter(motorSpeed(rightShootMotor)) + rpmToPowerConverter(motorSpeed(leftShootMotor))) / 2));
+            }
+            else
+            {
+                telemetry.addData("Motor Speeds are Aligned", "MotorOne === MotorTwo");
+            }
+        }
+        if(time <= 3.5 && time > 3)
+        {
+            whiskMotor.setPower(1);
+            leftShootMotor.setPower(.25);
+            rightShootMotor.setPower(.25);
+            if(motorSpeed(leftShootMotor) > motorSpeed(rightShootMotor))
+            {
+                telemetry.addData("Aligning Motor Speed", "MotorOne > MotorTwo");
+                leftShootMotor.setPower(leftShootMotor.getPower() - ((rpmToPowerConverter(motorSpeed(leftShootMotor)) - rpmToPowerConverter(motorSpeed(rightShootMotor))) / 2));
+                rightShootMotor.setPower(rightShootMotor.getPower() + ((rpmToPowerConverter(motorSpeed(leftShootMotor)) - rpmToPowerConverter(motorSpeed(rightShootMotor))) / 2));
+            }
+            else if (motorSpeed(leftShootMotor) < motorSpeed(rightShootMotor))
+            {
+                telemetry.addData("Aligning Motor Speed", "MotorTwo > MotorOne");
+                rightShootMotor.setPower(rightShootMotor.getPower() - ((rpmToPowerConverter(motorSpeed(rightShootMotor)) - rpmToPowerConverter(motorSpeed(leftShootMotor))) / 2));
+                leftShootMotor.setPower(leftShootMotor.getPower() + ((rpmToPowerConverter(motorSpeed(rightShootMotor)) + rpmToPowerConverter(motorSpeed(leftShootMotor))) / 2));
+            }
+            else
+            {
+                telemetry.addData("Motor Speeds are Aligned", "MotorOne === MotorTwo");
+            }
+        }
+        if(time > 3.5 && time <= 5)
+        {
+            whiskMotor.setPower(0);
+            leftShootMotor.setPower(.25);
+            rightShootMotor.setPower(.25);
+            if(motorSpeed(leftShootMotor) > motorSpeed(rightShootMotor))
+            {
+                telemetry.addData("Aligning Motor Speed", "MotorOne > MotorTwo");
+                leftShootMotor.setPower(leftShootMotor.getPower() - ((rpmToPowerConverter(motorSpeed(leftShootMotor)) - rpmToPowerConverter(motorSpeed(rightShootMotor))) / 2));
+                rightShootMotor.setPower(rightShootMotor.getPower() + ((rpmToPowerConverter(motorSpeed(leftShootMotor)) - rpmToPowerConverter(motorSpeed(rightShootMotor))) / 2));
+            }
+            else if (motorSpeed(leftShootMotor) < motorSpeed(rightShootMotor))
+            {
+                telemetry.addData("Aligning Motor Speed", "MotorTwo > MotorOne");
+                rightShootMotor.setPower(rightShootMotor.getPower() - ((rpmToPowerConverter(motorSpeed(rightShootMotor)) - rpmToPowerConverter(motorSpeed(leftShootMotor))) / 2));
+                leftShootMotor.setPower(leftShootMotor.getPower() + ((rpmToPowerConverter(motorSpeed(rightShootMotor)) + rpmToPowerConverter(motorSpeed(leftShootMotor))) / 2));
+            }
+            else
+            {
+                telemetry.addData("Motor Speeds are Aligned", "MotorOne === MotorTwo");
+            }
+        }
+        if(time > 5)
+        {
+            whiskMotor.setPower(0);
+            leftShootMotor.setPower(0);
+            rightShootMotor.setPower(0);
+            curState = BotState.INITIAL_TURN;
+        }
+    }
+
+
+    public int motorSpeed(DcMotor motor)
+    {
+        if(speedCheckTrigger == false)
+        {
+            speedCheckStartTime = time;
+            currentCounts = motor.getCurrentPosition();
+            speedCheckTrigger = true;
+        }
+        if(time <= speedCheckStartTime + 1)
+        {
+            currentCounts = motor.getCurrentPosition() - currentCounts;
+        }
+        else
+        {
+            return currentCounts;
+        }
+        return 0;
+    }
+
+    public double rpmToPowerConverter(int numToConvert)
+    {
+        double convertedNum = (double)(numToConvert / MAX_MOTOR_RPM);
+        return convertedNum;
+    }
+
+    public void alignMotorSpeed(DcMotor motorOne, DcMotor motorTwo)
+    {
+        if(motorSpeed(motorOne) > motorSpeed(motorTwo))
+        {
+            telemetry.addData("Aligning Motor Speed", "MotorOne > MotorTwo");
+            motorOne.setPower(motorOne.getPower() - ((rpmToPowerConverter(motorSpeed(motorOne)) - rpmToPowerConverter(motorSpeed(motorTwo))) / 2));
+            motorTwo.setPower(motorTwo.getPower() + ((rpmToPowerConverter(motorSpeed(motorOne)) - rpmToPowerConverter(motorSpeed(motorTwo))) / 2));
+        }
+        else if (motorSpeed(motorOne) < motorSpeed(motorTwo))
+        {
+            telemetry.addData("Aligning Motor Speed", "MotorTwo > MotorOne");
+            motorTwo.setPower(motorTwo.getPower() - ((rpmToPowerConverter(motorSpeed(motorTwo)) - rpmToPowerConverter(motorSpeed(motorOne))) / 2));
+            motorOne.setPower(motorOne.getPower() + ((rpmToPowerConverter(motorSpeed(motorTwo)) + rpmToPowerConverter(motorSpeed(motorOne))) / 2));
+        }
+        else
+        {
+            telemetry.addData("Motor Speeds are Aligned", "MotorOne === MotorTwo");
+        }
+    }
+}
